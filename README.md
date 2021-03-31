b1defa8ec23f1124c3cf589f6f1ad59e61b39b3c
iboyan <2073379083@qq.com>
2021-03-31 20:47:12 +0800

    ADD

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# kms
+# Kms服务端
+
+<span>wget --no-check-certificate https://raw.githubusercontent.com/iboyan/kms/main/kms-install.sh;chmod +x kms-install.sh;./kms-install.sh</span>
---
+++ kms-install.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+#apt-get update
+cd /root
+wget https://raw.githubusercontent.com/iboyan/kms/main/binaries.tar.gz
+tar zxf binaries.tar.gz
+cd /root/binaries/Linux/intel/static
+mv vlmcsdmulti-x64-musl-static /usr/local/KMS-server
+chmod u+x /usr/local/KMS-server
+cd /root && rm -rf binaries binaries.tar.gz
+apt-get install supervisor -y
+echo "[program:kms]
+command=/usr/local/KMS-server JiaShuCan
+autorestart=true
+autostart=true
+user=root" > /etc/supervisor/conf.d/kms.conf
+/etc/init.d/supervisor restart
+cd /root && rm -rf kms-install.sh
